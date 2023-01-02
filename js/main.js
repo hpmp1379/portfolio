a9675af3496943780b747473c65a9fb56cd6aa64
HPs <88412085+hpmp1379@users.noreply.github.com>
2023-01-02 18:21:45 +0900

    '0102'

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 // JQuery
 $(function () {
-  let n = 0;
-  let winH = $(window).height();
   let winW = $(window).width();
   $(window).resize(function () {
-    winH = $(window).height();
     winW = $(window).width();
     if (winW > 767) {
-      $(".menuWrap").fadeIn(300);
+      $(".menuWrap").css({ display: "block" });
+    } else if (winW < 768) {
+      $(".menuWrap").css({ display: "none" });
     }
   });
 
+  // menu show/hide (respon < 768)
+  // click X or menu to close
   $(".menuWrap")
     .find("#menuClose")
     .click(function () {
       $(".menuWrap").fadeOut(300);
     });
-  // $('').find('').click(function(){
-  //     $('').fadeIn(300);
-  // })
+  $(".bnwOnOff .burgerIcon").click(function () {
+    $(".menuWrap").fadeToggle(300);
+  });
 
   //   bnw On Off
-  $(".bnwOnOff").click(function () {
+  $(".bnwOnOff .bnwToggle").click(function () {
     $("body").toggleClass("bnw");
     $(".hdrWrap").toggleClass("bnw");
     $(".menuWrap").toggleClass("bnw");
